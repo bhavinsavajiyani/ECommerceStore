93b07744e6e772d77b58dd1a7e8c3029c2763c0c
Bhavin Savajiyani <bhavinsavajiyani9@gmail.com>
2024-06-30 21:23:11 +0530

    - Implement Client-Side feature of Shop Basket - Add new ng modules ("basket", "basket-routing", "checkout", "checkout-routing") - Add new ng components ("basket", "checkout", "order-totals") - Add new ng services ("basket", "checkout") - Add new interfaces [models] ("Basket", "BasketItem", "BasketTotals") & export class ("Basket") - Add new npm package ("cuid") - Update module ("app-routine") which now contains routes for basket & checkout modules - Update module ("shared") which now declares & exports ("OrderTotalsComponent") - Update root component ("app") which now injects ("BasketService") & utilizes the service to get basket (of a given "basket_id") from "localStorage" - Update component ("nav-bar") which now imports ("BasketService", "BasketItem") & add a method to get the count of items in basket - Update component ("product-details") which now imports ("BasketService", "take" [from rxjs]) & subscribe to observable which would get product count from basket via pipe 	- Add new methods ("incrementQuantity", "DecrementQuantity", "updateBasket", "get buttonText") - Update component ("product-item") which now imports & injects BasketService & utilizes it to add item to basket - Update html component ("nav-bar") which now contains routerLink to basket & the basket's item count now gets dynamically updated via async pipe - Update html component ("product-details") which now contains a heading that displays quantity of the current product in basket & on-click events for incrementing / decrementing / updating the quantity of current product in basket - Update html component ("product-item") which now contains on-click event to add an item to the basket - Add ability to automatically calculate order total amount based upon the items & their quantities added to the basket and ability to proceed to checkout page

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -3,6 +3,8 @@ import { Product } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
 import { BreadcrumbService } from 'xng-breadcrumb';
+import { BasketService } from 'src/app/basket/basket.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
@@ -11,8 +13,13 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 })
 export class ProductDetailsComponent implements OnInit {
   product?: Product;
+  quantity = 1;
+  quantityInBasket = 0;
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) {
+  constructor(private shopService: ShopService,
+    private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService,
+    private basketService: BasketService) {
     this.bcService.set('@productDetails', ' ');
   }
 
@@ -26,9 +33,45 @@ export class ProductDetailsComponent implements OnInit {
       next: product => {
         this.product = product;
         this.bcService.set('@productDetails', product.name);
+        this.basketService.basketSource$.pipe(take(1)).subscribe({
+          next: basket => {
+            const item = basket?.items.find(x => x.id === +productid);
+            if(item) {
+              this.quantity = item.quantity;
+              this.quantityInBasket = item.quantity;
+            }
+          }
+        });
       },
       error: error => console.log(error)
     });
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    this.quantity--;
+  }
+
+  updateBasket() {
+    if(this.product) {
+      if(this.quantity > this.quantityInBasket) {
+        const numberOfItemsToAddToBasket = this.quantity - this.quantityInBasket;
+        this.quantityInBasket += numberOfItemsToAddToBasket;
+        this.basketService.addItemToBasket(this.product, numberOfItemsToAddToBasket);
+      }
+      else {
+        const numberOfItemsToRemoveFromBasket = this.quantityInBasket - this.quantity;
+        this.quantityInBasket -= numberOfItemsToRemoveFromBasket;
+        this.basketService.removeItemFromBasket(this.product.id, numberOfItemsToRemoveFromBasket);
+      }
+    }
+  }
+
+  get buttonText() {
+    return this.quantityInBasket === 0 ? 'Add to Basket' : 'Update Basket'
   }
 
 }
