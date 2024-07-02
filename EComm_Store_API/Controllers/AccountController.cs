ed32cc4bdc516c632b9b569cb6949aa92258ea76
Bhavin Savajiyani <bhavinsavajiyani9@gmail.com>
2024-07-02 15:40:22 +0530

    - Implement additional account methods - Add new extension class ("UserManagerExtensions") - Add new data-transfer object class ("AddressDTO") - Add new test user to identity db - Update utility class ("MappingProfiles") which now uses auto-mapper to map properties of "Address" to "AddressDTO" & vice-versa ("ReverseMap") - Update controller class ("AccountController") which now injects ("IMapper") to constructor & implement additional methods to get current user, validate if user email already exists, get user address & update user address

--- EComm_Store_API/Controllers/AccountController.cs
+++ EComm_Store_API/Controllers/AccountController.cs
@@ -1,7 +1,11 @@
+using System.Security.Claims;
+using AutoMapper;
 using EComm_Store_API.DTOs;
 using EComm_Store_API.Errors;
+using EComm_Store_API.Extensions;
 using EComm_Store_Core.Entities.Identity;
 using EComm_Store_Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,15 +16,62 @@ namespace EComm_Store_API.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly ITokenService _tokenService;
+        private readonly IMapper _mapper;
 
         public AccountController(
             UserManager<AppUser> userManager,
             SignInManager<AppUser> signInManager,
-            ITokenService tokenService)
+            ITokenService tokenService,
+            IMapper mapper)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _tokenService = tokenService;
+            _mapper = mapper;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserDTO>> GetCurrentUser()
+        {
+            var user = await _userManager.FindUserByEmailFromClaimsPrincipal(User);
+
+            return new UserDTO
+            {
+                Email = user.Email,
+                Token = _tokenService.GenerateToken(user),
+                DisplayName = user.DisplayName,
+            };
+        }
+
+        // Useful for async validation on client-side to check if email already exists on server (Cannot allow new user to register with email that already exists)
+        [HttpGet("emailexists")]
+        public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery] string email)
+        {
+            return await _userManager.FindByEmailAsync(email) != null;
+        }
+
+        [Authorize]
+        [HttpGet("address")]
+        public async Task<ActionResult<AddressDTO>> GetUserAddress()
+        {
+            // var email = HttpContext.User?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.Email);
+
+            var user = await _userManager.FindUserByClaimsPrincipalWithAddressAsync(User);
+
+            return _mapper.Map<Address, AddressDTO>(user.Address);
+        }
+
+        [Authorize]
+        [HttpPut("address")]
+        public async Task<ActionResult<AddressDTO>> UpdateUserAddress(AddressDTO address)
+        {
+            var user = await _userManager.FindUserByClaimsPrincipalWithAddressAsync(User);
+            user.Address = _mapper.Map<AddressDTO, Address>(address);
+
+            var result = await _userManager.UpdateAsync(user);
+            if(result.Succeeded) return Ok(_mapper.Map<Address, AddressDTO>(user.Address));
+            return BadRequest("Couldn't Update the User!");
         }
 
         [HttpPost("login")]
