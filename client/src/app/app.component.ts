f08ecf3a0388d9fd97a0dece9a35daabb95954ed
Bhavin Savajiyani <bhavinsavajiyani9@gmail.com>
2024-07-03 21:12:05 +0530

    - Implement persistent login, form validation & reusable text input - Add new ng component ("text-input") that acts as a reusable component for text-input validation - Update module ("shared") which now contains: 	- Declaration for ("TextInputComponent") 	- Imports ("BsDropdownModule") 	- Exports ("BsDropdownModule", "TextInputComponent") - Update modules ("core", "account") which now import module ("shared") - Update service ("account") which now implements methods for loading current-user, login, register, check if email exists & logout - Update component ("login") which implements a form-group (comprising form-control objects) & a method to redirect user to shop's home-page - Update component ("nav-bar") which now injects account service into constructor - Update root component ("app") which would now load the current-user & basket upon init - Update interceptor ("loading") which now reduces intentionally added delay (to test loading-spinner functionality) from 1000ms to 10ms - Update html component ("login") which now implements a login form along with validations - Update html component ("nav-bar") which now: 	- Shows login & sign-up buttons only when user is logged out 	- Shows user's display name & a drop-down menu that includes options to view basket, view orders & logout, when user is logged in

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,19 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'SnowGearStore';
 
-  constructor(private basketService: BasketService) {}
+  constructor(private basketService: BasketService, private accountService: AccountService) {}
 
   ngOnInit(): void {
+    this.loadCurrentUser();
+    this.loadBasket();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    if(token) this.accountService.loadCurrentUser(token).subscribe();
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if(basketId) this.basketService.getBasket(basketId);
   }
